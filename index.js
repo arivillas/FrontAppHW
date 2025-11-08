6f536bfae1d7e487fac8ae561fa4946af42cd788
Andres <87373800+arivillas@users.noreply.github.com>
2025-11-07 23:28:46 -0500

    Update greeting message in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node Change 01!\n'
+  const msg = 'Hello Node Change 02 - Slack Notifications!\n'
   res.end(msg);
 });
 
